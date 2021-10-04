58b2a3405535fb62cbabb028bbfe22f5132d4c3f
Rod Fetalvero <botsot.felix@gmail.com>
2021-10-04 20:32:28 +0800

    Optimize imports

--- src/main/java/com/example/springbootmongodb/contollers/TodoController.java
+++ src/main/java/com/example/springbootmongodb/contollers/TodoController.java
@@ -1,15 +1,19 @@
 package com.example.springbootmongodb.contollers;
 
 import com.example.springbootmongodb.model.TodoDTO;
+import com.example.springbootmongodb.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.CollectionUtils;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
-
-
-import com.example.springbootmongodb.repository.TodoRepository;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 import java.util.List;
